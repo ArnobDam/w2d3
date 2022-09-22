548777d28a2d1101f2686a4fb4672dae62b1ea4f
Arnob <arnobdam@gmail.com>
2022-09-21 21:54:26 -0400

    tictactoe further methods added

--- arnob_dam_tictactoe_project/tic_tac_toe_v1/board.rb
+++ arnob_dam_tictactoe_project/tic_tac_toe_v1/board.rb
@@ -32,10 +32,75 @@ class Board
         end
     end
 
-    def print?
+    def print
         @board.each do |arr|
             print arr.join(" ")
             puts
         end
+    end
+
+    def win_row?(mark)
+        @board.each do |arr|
+            if arr.all? {|ele| ele == mark}
+                return true
+            end
+        end
+        false
+    end
+
+    def win_col?(mark)
+        (0...@board.length).each do |i|
+            count = 0
+            @board.each do |arr|
+                if arr[i] == mark
+                    count += 1
+                end
+            end
+            if count == @board.length
+                return true
+            end
+        end
+        false
+    end
+
+    def win_diagonal?(mark)
+        #diagonal starting at top left corner
+        count = 0
+        @board.each_with_index do |arr,i1|
+            arr.each_with_index do |ele,i2|
+                if i1 == i2
+                    if ele == mark
+                        count += 1
+                    end
+                end
+            end
+        end
+        if count == 3
+            return true
+        end
+        #false
+
+        #diagonal starting at top right corner
+        count = 0
+        @board.each_with_index do |arr,i1|
+            arr.each_with_index do |ele,i2|
+                if i1 == arr.length - 1 - i2
+                    if ele == mark
+                        count += 1
+                    end
+                end
+            end
+        end
+        if count == 3
+            return true
+        end
+        false
+    end
+
+    def win?(mark)
+        if win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+            return true
+        end
+        false
     end
 end
